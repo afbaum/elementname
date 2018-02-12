b8bacae55f2f6965ad44f5c5d1e0afaad2009c6d
Amanda Baum <amanda.baum01@gmail.com>
2018-02-12 18:26:46 -0500

    working on turning string into array

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ var element = [{name:"Hydrogen" , symbol:"H" , atiomicNumber:"1", weight:"1.008"
               {name:"Boron" , symbol:"B" , atiomicNumber:"5", weight:"10.81", state:"solid", metal:"metalloids"}
             ];
 var userName = "HeBeH"
+var namearray = [];
 
-for (i = 0; i < element.length; i++){
+for (i = 0; i < userName.length; i+=2){
+  console.log(userName[i]);
+}
+
+for (i = 0; i < element.length; i+=1){
   var number = userName.indexOf(element[i].symbol)
   if (number >= 0){
     document.write(element[i].name);
